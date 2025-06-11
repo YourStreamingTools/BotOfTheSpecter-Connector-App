f2a659b6d459c82ca78a59e08bf702afcd896cde
GFAUnDead <contact@gfaundead.stream>
2025-06-12 08:15:33 +1000

    Update BaseViewModel.cs

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -6,14 +6,14 @@ namespace BotOfTheSpecterOBSConnector.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(field, value))
                 return false;

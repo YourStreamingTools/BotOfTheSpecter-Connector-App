e108a659d05e085e88553fd9f7320da7d35a131c
GFAUnDead <contact@gfaundead.stream>
2025-06-12 10:24:22 +1000

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,19 +22,14 @@ namespace BotOfTheSpecterOBSConnector
             PasswordBox.Password = _viewModel.ServerPassword;
             Closing += (s, e) => _viewModel.Dispose();
         }
-
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            _viewModel.ServerPassword = PasswordBox.Password;
-        }
+        { _viewModel.ServerPassword = PasswordBox.Password; }
     }
-
     [ValueConversion(typeof(bool), typeof(SolidColorBrush))]
     public class BoolToColorConverter : IValueConverter
     {
         public string TrueColor { get; set; } = "#FF00FF00";
         public string FalseColor { get; set; } = "#FFFF0000";
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool boolValue)
@@ -44,10 +39,7 @@ namespace BotOfTheSpecterOBSConnector
             }
             return new SolidColorBrush(Colors.White);
         }
-
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        { throw new NotImplementedException(); }
     }
 }
